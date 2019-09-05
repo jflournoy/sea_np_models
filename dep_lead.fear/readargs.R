9d136fdddf1a3a8d8d6387e5bf8c92fa2e609f38
John Flournoy <john_flournoy@fas.harvard.edu>
2019-09-05 09:59:25 -0400

    new models

---
+++ dep_lead.fear/readargs.R
@@ -0,0 +1,9 @@
+
+cmdargs <- c("-m","mask.nii.gz", "--set1", "/mnt/stressdevlab/stress_pipeline/Group/FaceReactivity/NewNeuropoint/datafiles/setfilenames_fearGTcalm.txt",
+             "--setlabels1", "/mnt/stressdevlab/stress_pipeline/Group/FaceReactivity/NewNeuropoint/datafiles/depanxcov-midpoint5.csv", 
+             "--model", "model.R",
+             "--testvoxel", "10000",
+             "--output", "dep_lead.fear/dep_lead.fear.",
+             "--debugfile", "debug.Rdata",
+             "--slurmN", "60")
+
