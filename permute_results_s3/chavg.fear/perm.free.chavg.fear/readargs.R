866925c8334f5fdab25c6d861142ba6d0355e206
John Flournoy <john_flournoy@fas.harvard.edu>
2019-09-15 15:33:25 -0400

    chavg perms

---
+++ permute_results_s3/chavg.fear/perm.free.chavg.fear/readargs.R
@@ -0,0 +1,7 @@
+cmdargs <- c("-m","/net/holynfs01/srv/export/mclaughlin/share_root/stressdevlab/stress_pipeline/Group/FaceReactivity/NewNeuropoint/chavg.fear/make_perms/mask.nii.gz",
+"--set1", "/net/holynfs01/srv/export/mclaughlin/share_root/stressdevlab/stress_pipeline/Group/FaceReactivity/NewNeuropoint/datafiles/setfilenames_fearGTcalm.txt","--setlabels1", "/net/holynfs01/srv/export/mclaughlin/share_root/stressdevlab/stress_pipeline/Group/FaceReactivity/NewNeuropoint/datafiles/stresscov-midpoint5.csv",
+"--model","/net/holynfs01/srv/export/mclaughlin/share_root/stressdevlab/stress_pipeline/Group/FaceReactivity/NewNeuropoint/chavg.fear/make_perms/permute_free_model.R",
+"--output","perm.free.chavg.fear/perm.free.chavg.fear",
+debug.Rdata
+"--slurmN", "60"
+)
