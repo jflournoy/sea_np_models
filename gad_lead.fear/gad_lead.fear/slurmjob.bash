d4f0974dde7f84ea51bcf902f5ee2e2514ca3cac
John Flournoy <john_flournoy@fas.harvard.edu>
2019-10-27 11:06:55 -0400

    slurmjobs

---
+++ gad_lead.fear/gad_lead.fear/slurmjob.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+#Slurm submission options
+#LOOK AT THESE AND EDIT TO OVERRIDE FOR YOUR JOB
+#SBATCH -p ncf_holy
+#SBATCH --mem 4000
+#SBATCH --time 0-6:00
+#SBATCH --mail-type=END
+#SBATCH -o npoint_%A_%a.out
+#SBATCH -o npoint_%A_%a.err
+export OMP_NUM_THREADS=1
+MODEL=/net/holynfs01/srv/export/mclaughlin/share_root/stressdevlab/stress_pipeline/Group/FaceReactivity/NewNeuropoint/gad_lead.fear/model.R
+DESIGNMAT=gad_lead.fear.designmat.rds
+num=$(printf "%04d" $SLURM_ARRAY_TASK_ID)
+npointrun -m gad_lead.fear.${num}.nii.gz --model ${MODEL} -d ${DESIGNMAT}
+
+
