976d7f500779e83de1b3730cf4f76c42b14ce91c
John Flournoy <john_flournoy@fas.harvard.edu>
2019-10-27 11:13:14 -0400

    more R files

---
+++ permute_results_s3/panasneg.fear/perm.free.panasneg.fear/perm.free.panasneg.fear/readargs.R
@@ -0,0 +1,7 @@
+cmdargs <- c("-m","/net/holynfs01/srv/export/mclaughlin/share_root/stressdevlab/stress_pipeline/Group/FaceReactivity/NewNeuropoint/panasneg.fear/perm.free.panasneg.fear/mask.nii.gz",
+"--set1", "/net/holynfs01/srv/export/mclaughlin/share_root/stressdevlab/stress_pipeline/Group/FaceReactivity/NewNeuropoint/datafiles/setfilenames_fearGTcalm.txt","--setlabels1", "/net/holynfs01/srv/export/mclaughlin/share_root/stressdevlab/stress_pipeline/Group/FaceReactivity/NewNeuropoint/datafiles/depanxcov-midpoint5.csv",
+"--model","/net/holynfs01/srv/export/mclaughlin/share_root/stressdevlab/stress_pipeline/Group/FaceReactivity/NewNeuropoint/panasneg.fear/perm.free.panasneg.fear/permute_free_model.R",
+"--output","perm.free.panasneg.fear/perm.free.panasneg.fear.",
+debug.Rdata
+"--slurmN", "60"
+)
